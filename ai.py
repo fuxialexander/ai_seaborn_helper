9ef24a28a54bc04fa8327c84526d2eeadbced00c
Xi Fu <fuxialexander@gmail.com>
2023-03-27 10:38:23 -0400

    update

--- ai.py
+++ ai.py
@@ -1,4 +1,3 @@
-
 import openai
 import pandas as pd
 import seaborn as sns
@@ -14,21 +13,33 @@ def load_dataframe(csv_path):
     df = pd.read_csv(csv_path)
     return df
 
-def send_dfframe_to_gpt(df):
-    head = df.head().to_string()
+def describe_df(filename):
+    df = load_dataframe(filename)
+    head = df.head(2).to_string()
     summary = df.describe().to_string()
+    prompt = f"Describe what's the data in this table with summary\n{head}\n+{summary}\n;"
+    response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=prompt,
+    temperature=0,
+    max_tokens=1000,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    stop=["\"\"\""]
+    )
+    code = response.choices[0].text.strip()
+    return code
 
-    prompt = f"Here is the head of the dataframe called 'df' :\n{head}\n\n;"
-    return prompt
-
-def generate_plot_code(prompt, x, y, plot_type):
-    prompt += f"Generate Python code using seaborn to create a {plot_type} plot with x-axis as '{x}' and y-axis as '{y}'."
+def generate_plot_code(prompt, x, y, plot_type, filename, first=False):
+    if first:
+        prompt = f"You are a precise python code generator. You only output code, not other texts. Generate Python code that load file called {filename} in pandas, then a using seaborn to create a {plot_type} plot with x-axis as '{x}' and y-axis as '{y}'." + prompt
     print(prompt)
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
         temperature=0,
-        max_tokens=64,
+        max_tokens=1000,
         top_p=1.0,
         frequency_penalty=0.0,
         presence_penalty=0.0,
@@ -38,15 +49,12 @@ def generate_plot_code(prompt, x, y, plot_type):
     code = response.choices[0].text.strip()
     return code
 
-def execute_code(code, df):
+def execute_code(code):
     old_stdout = sys.stdout
     redirected_output = sys.stdout = io.StringIO()
-    local_vars = {"df": df, "sns": sns}
     try:
-        exec(code, None, local_vars)
+        exec(code)
         # savefig in "plot.png"
-        plt.show()
-        plt.savefig("plot.png")
         result = "success"
     except Exception as e:
         result = "error"
@@ -58,20 +66,24 @@ def execute_code(code, df):
     return result, output
 
 def auto_plot(filename, x, y, plot_type, additional_prompt=""):
-    df = load_dataframe(filename)
-    prompt = additional_prompt + send_dfframe_to_gpt(df)
     success = False
-
+    i = 0
     while not success:
-        code = generate_plot_code(prompt, x, y, plot_type)
-        print(code)
-        result, output = execute_code(code,df)
-
+        if i==0:
+            prompt = additional_prompt
+        code = generate_plot_code(prompt, x, y, plot_type, filename, first=(i==0))
+        # ask for additional prompt from user
+        result, output = execute_code(code)
+        # keep only 3rd line and last line of output
         if result == "success":
             success = True
         else:
-            prompt = f"\n\nError message:\n{output}\n\n"
+            output = output.splitlines()[2] + output.splitlines()[-1]
+            print(output)
+            prompt = f"\nError message:\n{output}\n"
+            additional_prompt = input("Additional prompt to help the AI generate better code: ")
+            prompt += additional_prompt
             prompt += "Please provide a new code to generate the plot as described above."
-            prompt +=additional_prompt
-
+        
+        i += 1
     return code
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 #%%
-import pandas as pd
-from ai import load_dataframe, auto_plot
+from ai import auto_plot, describe_df
 
 csv_path = "HPI_master.csv"
 x = "hpi_flavor"
 y = "index_sa"
 plot_type = "boxplot"
 #%%
-code = auto_plot(csv_path, x, y, plot_type, additional_prompt="Make sure to use the 'hue' parameter to color the plot by year column in the table; return only the code, not any other text. Do not show legend. Remeber the data frame is called 'df'.")
-print(f"Plotting code: {code}")
+description = describe_df(csv_path)
+#%%
+# print description, split into 60 characters lines
+for i in range(0, len(description), 60):
+    print(description[i:i+60])
+#%%
+code = auto_plot(csv_path, x, y, plot_type, 
+    additional_prompt="""
+    Create a figure with figsize=(10,10) before plotting;
+    Make sure to use the 'hue' parameter to color the plot by 'yr' column in the table; 
+    Hide the legend after plot.""")
 
 # %%
